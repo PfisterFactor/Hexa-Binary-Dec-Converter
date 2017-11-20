d564800983c4941481ecc06450f4309a58cb2ae2
EJ <EJPfister55@gmail.com>
2017-11-19 23:18:32 -0600

    First Commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace HexaBinaryDecConverter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.DecimalCheckIn = new System.Windows.Forms.CheckBox();
+            this.HexaCheckIn = new System.Windows.Forms.CheckBox();
+            this.BinaryCheckIn = new System.Windows.Forms.CheckBox();
+            this.BinaryCheckOut = new System.Windows.Forms.CheckBox();
+            this.HexaCheckOut = new System.Windows.Forms.CheckBox();
+            this.DecimalCheckOut = new System.Windows.Forms.CheckBox();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.Arrow = new System.Windows.Forms.PictureBox();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Arrow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(12, 78);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(100, 20);
+            this.InputBox.TabIndex = 0;
+            this.InputBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputBox_KeyPress);
+            // 
+            // DecimalCheckIn
+            // 
+            this.DecimalCheckIn.AutoSize = true;
+            this.DecimalCheckIn.Checked = true;
+            this.DecimalCheckIn.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.DecimalCheckIn.Location = new System.Drawing.Point(12, 9);
+            this.DecimalCheckIn.Name = "DecimalCheckIn";
+            this.DecimalCheckIn.Size = new System.Drawing.Size(64, 17);
+            this.DecimalCheckIn.TabIndex = 1;
+            this.DecimalCheckIn.Text = "Decimal";
+            this.DecimalCheckIn.UseVisualStyleBackColor = true;
+            this.DecimalCheckIn.CheckedChanged += new System.EventHandler(this.AnyBoxChecked);
+            // 
+            // HexaCheckIn
+            // 
+            this.HexaCheckIn.AutoSize = true;
+            this.HexaCheckIn.Location = new System.Drawing.Point(12, 32);
+            this.HexaCheckIn.Name = "HexaCheckIn";
+            this.HexaCheckIn.Size = new System.Drawing.Size(87, 17);
+            this.HexaCheckIn.TabIndex = 2;
+            this.HexaCheckIn.Text = "Hexadecimal";
+            this.HexaCheckIn.UseVisualStyleBackColor = true;
+            this.HexaCheckIn.CheckedChanged += new System.EventHandler(this.AnyBoxChecked);
+            // 
+            // BinaryCheckIn
+            // 
+            this.BinaryCheckIn.AutoSize = true;
+            this.BinaryCheckIn.Location = new System.Drawing.Point(12, 55);
+            this.BinaryCheckIn.Name = "BinaryCheckIn";
+            this.BinaryCheckIn.Size = new System.Drawing.Size(55, 17);
+            this.BinaryCheckIn.TabIndex = 3;
+            this.BinaryCheckIn.Text = "Binary";
+            this.BinaryCheckIn.UseVisualStyleBackColor = true;
+            this.BinaryCheckIn.CheckedChanged += new System.EventHandler(this.AnyBoxChecked);
+            // 
+            // BinaryCheckOut
+            // 
+            this.BinaryCheckOut.AutoSize = true;
+            this.BinaryCheckOut.Location = new System.Drawing.Point(285, 55);
+            this.BinaryCheckOut.Name = "BinaryCheckOut";
+            this.BinaryCheckOut.Size = new System.Drawing.Size(55, 17);
+            this.BinaryCheckOut.TabIndex = 6;
+            this.BinaryCheckOut.Text = "Binary";
+            this.BinaryCheckOut.UseVisualStyleBackColor = true;
+            this.BinaryCheckOut.CheckedChanged += new System.EventHandler(this.AnyBoxChecked);
+            // 
+            // HexaCheckOut
+            // 
+            this.HexaCheckOut.AutoSize = true;
+            this.HexaCheckOut.Location = new System.Drawing.Point(285, 32);
+            this.HexaCheckOut.Name = "HexaCheckOut";
+            this.HexaCheckOut.Size = new System.Drawing.Size(87, 17);
+            this.HexaCheckOut.TabIndex = 5;
+            this.HexaCheckOut.Text = "Hexadecimal";
+            this.HexaCheckOut.UseVisualStyleBackColor = true;
+            this.HexaCheckOut.CheckedChanged += new System.EventHandler(this.AnyBoxChecked);
+            // 
+            // DecimalCheckOut
+            // 
+            this.DecimalCheckOut.AutoSize = true;
+            this.DecimalCheckOut.Location = new System.Drawing.Point(285, 9);
+            this.DecimalCheckOut.Name = "DecimalCheckOut";
+            this.DecimalCheckOut.Size = new System.Drawing.Size(64, 17);
+            this.DecimalCheckOut.TabIndex = 4;
+            this.DecimalCheckOut.Text = "Decimal";
+            this.DecimalCheckOut.UseVisualStyleBackColor = true;
+            this.DecimalCheckOut.CheckedChanged += new System.EventHandler(this.AnyBoxChecked);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(272, 78);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(100, 20);
+            this.OutputBox.TabIndex = 8;
+            this.OutputBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.OutputBox_KeyPress);
+            // 
+            // Arrow
+            // 
+            this.Arrow.Image = global::HexaBinaryDecConverter.Properties.Resources.Arrow;
+            this.Arrow.Location = new System.Drawing.Point(139, 12);
+            this.Arrow.Name = "Arrow";
+            this.Arrow.Size = new System.Drawing.Size(100, 50);
+            this.Arrow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Arrow.TabIndex = 9;
+            this.Arrow.TabStop = false;
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConvertButton.Location = new System.Drawing.Point(139, 68);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(100, 30);
+            this.ConvertButton.TabIndex = 10;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 108);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.Arrow);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.BinaryCheckOut);
+            this.Controls.Add(this.HexaCheckOut);
+            this.Controls.Add(this.DecimalCheckOut);
+            this.Controls.Add(this.BinaryCheckIn);
+            this.Controls.Add(this.HexaCheckIn);
+            this.Controls.Add(this.DecimalCheckIn);
+            this.Controls.Add(this.InputBox);
+            this.Name = "MainForm";
+            this.Text = "Conversion";
+            ((System.ComponentModel.ISupportInitialize)(this.Arrow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.CheckBox DecimalCheckIn;
+        private System.Windows.Forms.CheckBox HexaCheckIn;
+        private System.Windows.Forms.CheckBox BinaryCheckIn;
+        private System.Windows.Forms.CheckBox BinaryCheckOut;
+        private System.Windows.Forms.CheckBox HexaCheckOut;
+        private System.Windows.Forms.CheckBox DecimalCheckOut;
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.PictureBox Arrow;
+        private System.Windows.Forms.Button ConvertButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HexaBinaryDecConverter
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InputBox.CharacterCasing = CharacterCasing.Upper;
+        }
+        private void uncheckCheckboxes(bool inputOrOutput)
+        {
+            if (inputOrOutput)
+            {
+                DecimalCheckIn.Checked = false;
+                HexaCheckIn.Checked = false;
+                BinaryCheckIn.Checked = false;
+            }
+            else
+            {
+                DecimalCheckOut.Checked = false;
+                HexaCheckOut.Checked = false;
+                BinaryCheckOut.Checked = false;
+            }
+        }
+
+        private void AnyBoxChecked(object sender, EventArgs e)
+        {
+          var senderAsCheckbox = sender as CheckBox;
+
+          if (senderAsCheckbox.Checked != true) return;
+
+          if (senderAsCheckbox.Name.EndsWith("In"))
+          {
+            uncheckCheckboxes(true);
+            InputBox.Clear();
+            senderAsCheckbox.CheckedChanged -= AnyBoxChecked;
+            senderAsCheckbox.Checked = true;
+            senderAsCheckbox.CheckedChanged += AnyBoxChecked;
+          }
+          else if (senderAsCheckbox.Name.EndsWith("Out"))
+          {
+            uncheckCheckboxes(false);
+            senderAsCheckbox.CheckedChanged -= AnyBoxChecked;
+            senderAsCheckbox.Checked = true;
+            senderAsCheckbox.CheckedChanged += AnyBoxChecked;
+          }
+          else
+          {
+            Console.WriteLine("Error!");
+            throw new Exception("Text.EndsWith didn't return properly!");
+          }
+        }
+
+        private void OutputBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+          if (InputBox.Text == "") return;
+
+            if (DecimalCheckIn.Checked)
+            {
+                if (DecimalCheckOut.Checked)
+                {
+                    OutputBox.Text = InputBox.Text;
+                }
+                else if (HexaCheckOut.Checked)
+                {
+                    OutputBox.Text = Conversion.decimalToHexa(Convert.ToInt32(InputBox.Text));
+                }
+                else if (BinaryCheckOut.Checked)
+                {
+                    OutputBox.Text = Conversion.decimalToBinary(Convert.ToInt32(InputBox.Text));
+                }
+            }
+            else if (HexaCheckIn.Checked)
+            {
+                if (DecimalCheckOut.Checked)
+                {
+                    OutputBox.Text = Conversion.hexaToDecimal(InputBox.Text).ToString();
+                }
+                else if (HexaCheckOut.Checked)
+                {
+                    OutputBox.Text = InputBox.Text;
+
+                }
+                else if (BinaryCheckOut.Checked)
+                {
+                    OutputBox.Text = Conversion.hexaToBinary(InputBox.Text);
+                }
+            }
+            else if (BinaryCheckIn.Checked)
+            {
+                if (DecimalCheckOut.Checked)
+                {
+                    OutputBox.Text = Conversion.binaryToDecimal(InputBox.Text).ToString();
+                }
+                else if (HexaCheckOut.Checked)
+                {
+                    OutputBox.Text = Conversion.binaryToHexa(InputBox.Text);
+                }
+                else if (BinaryCheckOut.Checked)
+                {
+                    OutputBox.Text = InputBox.Text;
+
+                }
+            }
+        }
+
+        private void InputBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (DecimalCheckIn.Checked)
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            }
+            else if (HexaCheckIn.Checked)
+            {
+                if (((TextBox)sender).Text.Length > 8 && !char.IsControl(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    if (!Conversion.hexadecimalTable.ContainsValue(Char.ToUpper(e.KeyChar)))
+                    {
+                        e.Handled = true;
+                    }
+
+                }
+            }
+            else if (BinaryCheckIn.Checked)
+            {
+                if (((TextBox)sender).Text.Length >= 31 && !char.IsControl(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+                if (!(e.KeyChar == '0' || e.KeyChar == '1') && !char.IsControl(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+    public static class Conversion {
+        public static Dictionary<int,char> hexadecimalTable = new Dictionary<int, char>()
+        {
+            {10,'A'},
+            {11,'B'},
+            {12,'C'},
+            {13,'D'},
+            {14,'E'},
+            {15,'F'}
+        };
+        public static Dictionary<char, int> inversedHexadecimalTable = new Dictionary<char, int>()
+        {
+            {'A',10},
+            {'B',11},
+            {'C',12},
+            {'D',13},
+            {'E',14},
+            {'F',15}
+        };
+        private static Tuple<int, int> divideRemainder(int a, int b)
+        {
+            int quotient = a / b;
+            int remainder = a % b;
+            return new Tuple<int, int>(quotient, remainder);
+        }
+        private static String getHexa(int a) //Returns a if a is < 10, elsewise returns the apropriate hexadecimal letter for that number. Depends on hexadecimalTable.
+        {
+          return a > 9 ? hexadecimalTable[a].ToString() : a.ToString();
+        }
+
+      public static long hexaToDecimal(String hexaAsString) // Converts a hexadecimal number in a string to a 32 bit integer, 64bit int is used to prevent overflow,
+        {
+            Int64 convertedHexa = 0;
+            if (hexaAsString.Length > 8)
+            {
+                Console.WriteLine("error");
+                return -1;
+            }
+            var count = 0;
+            for (int i = hexaAsString.Length - 1; i >= 0; i--)
+            {
+                if (hexadecimalTable.ContainsValue(hexaAsString[i]))
+                {
+                    int hexaletterInDecimal = inversedHexadecimalTable[hexaAsString[i]];
+                    convertedHexa += (hexaletterInDecimal * (Int64)Math.Pow((double)16, (double)count));
+                }
+                else
+                {
+                    Int64 number = ((Int64)Char.GetNumericValue(hexaAsString[i]) * (Int64)Math.Pow((double)16, (double)count));
+                    convertedHexa += number;
+                }
+                count++;
+            }
+            if (convertedHexa > Int32.MaxValue)
+            {
+                Console.WriteLine("error");
+                return -1;
+            }
+            else return convertedHexa;
+
+        }
+        public static String decimalToHexa(Int32 Decimal) //Converts a decimal to a hexadecimal number in a string, depends on "getHexa" and "divideRemainder"
+        {
+            string convertedDecimal = "";
+            Int32 editDecimal = Decimal; // We perform our operations on this to not screw up the original data.
+            while (true)
+            {
+                var divideOperation = divideRemainder(editDecimal, 16);
+                convertedDecimal += getHexa(divideOperation.Item2);
+                editDecimal = divideOperation.Item1;
+                if (divideOperation.Item1 == 0)
+                    break;
+            }
+            var flippedHexadecimal = convertedDecimal.Reverse<char>();
+            return new String(convertedDecimal.Reverse<char>().ToArray());
+        }
+
+        public static long binaryToDecimal(String binaryAsString) // Converts a string of binary to a 32 bit integer, 64 bit int is used to prevent overflow.
+        {
+            Int64 convertedBinary = 0;
+            if (binaryAsString.Length > 63)
+            {
+                return -1;
+            }
+            int count = 0;
+            for (int i = binaryAsString.Length - 1; i >= 0; i--)
+            {
+
+                if (binaryAsString[i] == '1')
+                {
+                    convertedBinary += (Int64)Math.Pow((double)2, (double)count);
+                }
+                count++;
+            }
+            return convertedBinary;
+        }
+        public static String decimalToBinary(Int32 Decimal) // Converts an Int32 to binary.
+        {
+            string convertedDecimal = "";
+            Int64 editDecimal = Decimal; // We perform our operations on this to not screw up the original data.
+            for (int i = 31; i >= 0; i--)
+            {
+                Int64 TwoToTheithPower = (Int64)Math.Pow((double)2, (double)i);
+                if (editDecimal - TwoToTheithPower >= 0)
+                {
+                    editDecimal -= TwoToTheithPower;
+                    convertedDecimal += "1";
+                }
+                else
+                {
+                    convertedDecimal += "0";
+                }
+            }
+
+            for (int i = 0; i <= convertedDecimal.Length - 1; i++) // Trims the trailing zeroes to get a much cleaner (and shorter) result.
+            {
+                if (convertedDecimal[i] == '1')
+                {
+                    convertedDecimal = convertedDecimal.Remove(0, i);
+                    break;
+                }
+            }
+            return convertedDecimal;
+        }
+
+        public static String binaryToHexa(String binaryAsString) //Converts a binary number in a string to hexadecimal number in a string. Depends on "decimalToHexa" and "binaryToDecimal" cause i'm lazy.
+        {
+            return decimalToHexa((Int32)binaryToDecimal(binaryAsString));
+        }
+        public static String hexaToBinary(String hexaAsString) //Converts a hexadecimal number in a string to a binary number in a string. Depends on "hexaToDecimal" and "decimalToBinary" cause i'm lazy.
+        {
+            return decimalToBinary((Int32)hexaToDecimal(hexaAsString));
+        }
+
+    }
+    //Note: I know that C# has a built in hexadecimal/binary/decimal conversion. This is for learning.
+
+}
